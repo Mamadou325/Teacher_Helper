e7d5925572d51c38a9c752192950ea6246d15236
Mamadou325 <mamadouwague805@gmail.com>
2024-10-25 16:10:46 +0000

    Index page.

---
+++ HWindex.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Grading Tool</title>
+</head>
+<body>
+    <h1>Grading Tool for Teachers</h1>
+    <a href="add_grades.php">Add Grades for Students</a>
+    <br><br>
+    <a href="view_grades.php">View Final Grades</a>
+</body>
+</html>
